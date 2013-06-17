998d71797ac2631a3d4f508b77a1b15a9af70ab8
codejie <codejie@gmail.com>
2013-06-17 23:50:23 +0800

    initial

---
+++ src/jie/android/bigscreen/view/BaseViewGroup.java
@@ -0,0 +1,20 @@
+package jie.android.bigscreen.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class BaseViewGroup extends ViewGroup {
+
+	public BaseViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
